e18b2fd2800a2c5275e38ec2bb3a9bae4492a2ad
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-14 23:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  if (typeof num !== 'number' || isNaN(num)) {
+    return 'Invalid input';
+  }
+
+  // Use toString() with radix 2 to convert to binary
+  const binary = num.toString(2);
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
